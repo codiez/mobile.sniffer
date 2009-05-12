8b961e147b438fac06fc11d096e410a63e855116
mikko@redinnovation.com <mikko@redinnovation.com@edb12760-3f46-11de-b438-597f59cd7555>
2009-05-12 23:49:56 +0000

    git-svn-id: http://mobilesniffer.googlecode.com/svn/trunk@5 edb12760-3f46-11de-b438-597f59cd7555

--- mobile.sniffer/mobile/sniffer/apexvertex/dump_settings.py
+++ mobile.sniffer/mobile/sniffer/apexvertex/dump_settings.py
@@ -2,9 +2,7 @@
 
     Django configuration file for pre-existing Apex Vertex MySQL databse.
 
-    MySQL SQL dump distribution settings.
-    
-    Bind Django models to existing MySQL table. 
+    MySQL SQL dump distribution settings - read MySQL database and generate database neutral Django JSON dump from the data using these settings.
 
 """
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -13,8 +11,6 @@ DATABASE_USER = 'apex'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'apex'             # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-HANDSET_HACK = True
 
 INSTALLED_APPS = (    
     'mobile.sniffer.apexvertex',
