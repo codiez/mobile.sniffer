a23b0776af4a58cfae8189919b4091178a5e5c25
antti.moilanen@twinapex.com <antti.moilanen@twinapex.com@edb12760-3f46-11de-b438-597f59cd7555>
2009-11-25 13:18:25 +0000

    added xml header. removed whitespaces before and after the xml values as s60 (N6110) could not open the landmark with them. Also fixed some invalid xml code
    git-svn-id: http://mobilesniffer.googlecode.com/svn/trunk@17 edb12760-3f46-11de-b438-597f59cd7555

--- mobile.heurestics/mobile/heurestics/poi.py
+++ mobile.heurestics/mobile/heurestics/poi.py
@@ -17,66 +17,48 @@ def nokia_poi(name=None, lng=None, lat=None, link=None, street=None,
     You need to set response content type to application/vnd.nokia.landmarkcollection+xml; charset=utf-8
     """
     buf = StringIO()
+    print >> buf, """<?xml version="1.0" encoding="UTF-8"?>"""
     print >> buf, """<lm:lmx xmlns:lm="http://www.nokia.com/schemas/location/landmarks/1/0/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.nokia.com/schemas/location/landmarks/1/0/ lmx.xsd">"""
     print >> buf, "<lm:landmark>"
 
     if name:
-        print >> buf, "<lm:name>"
-        print >> buf, name
-        print >> buf, "</lm:name>"
+        print >> buf, "<lm:name>%s</lm:name>" % name
 
     if lng != None:
-        print >> buf, "<<lm:coordinates>"
-        print >> buf, "        <lm:latitude>"
-        print >> buf, lat
-        print >> buf, "        </lm:latitude>"
-        print >> buf, "        <lm:longitude>"
-        print >> buf, lng
-        print >> buf, "        </lm:longitude>"
-        print >> buf, "        </lm:coordinates>"
+        print >> buf, "<lm:coordinates>"
+        print >> buf, "  <lm:latitude>%s</lm:latitude>" % lat
+        print >> buf, "  <lm:longitude>%s</lm:longitude>" % lng
+        print >> buf, "</lm:coordinates>"
 
     if street or country or city or postal_code or phone_number:
         print >> buf, "<lm:addressInfo>"
         if country:
-            print >> buf, "<lm:country>"
-            print >> buf, country
-            print >> buf, "</lm:country>"
+            print >> buf, "<lm:country>%s</lm:country>" % country
 
         if city:
-            print >> buf, "<lm:city>"
-            print >> buf, city
-            print >> buf, "</lm:city>"
+            print >> buf, "<lm:city>%s</lm:city>" % city
 
         if postal_code:
-            print >> buf, "<lm:postalCode>"
-            print postal_code
-            print >> buf, "</lm:postalCode>"
+            print >> buf, "<lm:postalCode>%s</lm:postalCode>" % postal_code
 
         if street:
-            print >> buf, "<lm:street>"
-            print >> buf, street
-            print >> buf, "</lm:street>"
+            print >> buf, "<lm:street>%s</lm:street>" % street
 
         if phone_number:
-            print >> buf, "<lm:phoneNumber>"
-            print >> buf, phone_number
-            print >> buf, "</lm:phoneNumber>"
-            print >> buf, "</lm:addressInfo>"
+            print >> buf, "<lm:phoneNumber>%s</lm:phoneNumber>" % phone_number
+        
+        print >> buf, "</lm:addressInfo>"
 
     if link:
         print >> buf, "<lm:mediaLink>"
-        print >> buf, "<lm:url>"
-        print >> buf, link
-        print >> buf, "</lm:url>"
+        print >> buf, "<lm:url>%s</lm:url>" % link
         print >> buf, "</lm:mediaLink>"
         print >> buf, "<lm:category>"
-        print >> buf, "<lm:id>"
-        print >> buf, "10000"
-        print >> buf, "</lm:id>"
+        print >> buf, "<lm:id>10000</lm:id>"
+        
         if media_name:
-            print >> buf, "<lm:name>"
-            print >> buf, media_name
-            print >> buf, "</lm:name>"
+            print >> buf, "<lm:name>%s</lm:name>" % media_name
+        
         print >> buf, "</lm:category>"
 
     print >> buf, "</lm:landmark>"
